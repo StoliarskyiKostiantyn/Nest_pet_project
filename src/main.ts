ace2caf08c9155e257f5032a71cd97f860183053
Kostia Stoliarskiy <saintkos117@gmail.com>
2025-03-01 15:37:51 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { logger } from 'common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
+  const config = new DocumentBuilder()
+    .setTitle('Nest pet project')
+    .setDescription('The API description')
+    .setVersion(process.env.npm_package_version ?? '1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
